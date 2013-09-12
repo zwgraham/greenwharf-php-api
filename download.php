290048c00cdc1bf836141bbf5241662258d04142
Zachary W. Graham <zack.graham@gmail.com>
2013-09-11 18:39:10 -0700

    most of download api finished, save for tar.gz and wind

--- download.php
+++ download.php
@@ -1,12 +1,15 @@
 <?php
 
 /* File:  history.php
- * Author:  Timothy Pace
- * Creation Date: 8/20/2013
- * Purpose: Retrieve data points from the greenwharf database.
+ * Author: Zachary Graham
+ * Creation Date: 9/5/2013
+ * Purpose: Create and return a csv file of wharf data
  *
  * Parameters: start - UTC formatted date string
  *             end - UTC formatted date string
+ *             type - (wind,solar,weather,all)
+ *
+ * Response:
  *
  * Response: If only start is given, the response is all data points between
  *           start and current time.  If only end is given, data points from
@@ -14,12 +17,15 @@
  *           and end are given.  If no arguments are given, the response is defaulted
  *           to the last two weeks.
  *
- *           JSON object echoed has fields (windSpeed, pyro, turbineAmps, windDir) with
- *           data formatted for highcharts.
+ *           type must be: solar, wind, weather, all. Currently solar and weather are 
+ *           implemented
+ *
  */
+
+
 function query_and_emit_csv($fobj, $query) {
-    $rows = mysql_query($query);
-    while( $row = mysql_fetch_assoc($rows) ) fputcsv($fobj, array_values($row));
+    $rows = mysql_query($query) or die("Query Error: ". mysql_error());
+    while( $row = mysql_fetch_assoc($rows) ) fputcsv($fobj, array_values($row), $delimeter=',', $enclosure=chr(0));
 }
 
 function make_weather_csv( $fobj, $start_ts=NULL, $end_ts=NULL) {
@@ -35,9 +41,9 @@ function make_weather_csv( $fobj, $start_ts=NULL, $end_ts=NULL) {
     if ($start_ts!=NULL) {
        if ($end_ts!=NULL) {
            $query = "SELECT " . join(', ', array_keys($columnToHeaderMap)) .
-              " FROM wharf_data WHERE utime >= " . $start_ts . " and utime <= ". $end_ts ." ORDER BY utime asc";
-       }
-       else {
+               " FROM wharf_data WHERE utime >= " . $start_ts . " and utime <= ". $end_ts .
+               " ORDER BY utime asc";
+       } else {
            $query = "SELECT " . join(', ', array_keys($columnToHeaderMap)) .
               " FROM wharf_data WHERE utime >= " . $start_ts . " ORDER BY utime asc";
        }
@@ -48,20 +54,18 @@ function make_weather_csv( $fobj, $start_ts=NULL, $end_ts=NULL) {
         $query = "SELECT " . join(', ', array_keys($columnToHeaderMap)) .
             " FROM wharf_data WHERE utime >= " . (time()-24*3600*14) . " ORDER BY utime asc";
     }
-
-    //query_and_emit_csv($fobj, $query);
-
+    query_and_emit_csv($fobj, $query);
 }
 
 function make_solar_csv( $fobj, $start_ts, $end_ts){
-/*    $columnToHeaderMap = [
-        '' => '',
-        '' => '',
-        '' => '',
-        '' => ''
-        ];*/
-    $columnToHeaderMap = array();
-    fputcsv($fobj, array_value($columnToHeaderMap));
+    $columnToHeaderMap = [
+        'Date_UTC' => 'Time (UTC)',
+        'VoltsIn' => 'PV Voltage',
+        'AmpsIn' => 'PV Current',
+        'BatVolts' => 'Battery Voltage',
+        'AmpsOut' => 'Charging Current'
+        ];
+    fputcsv($fobj, array_values($columnToHeaderMap));
 
     if ($start_ts!=NULL){
         if ($end_ts!=NULL) {
@@ -76,9 +80,9 @@ function make_solar_csv( $fobj, $start_ts, $end_ts){
             " FROM solar_data WHERE utime <= " . $end_ts . " ORDER BY utime asc";
     } else {
         $query = "SELECT " . join(', ', array_keys($columnToHeaderMap)) .
-            " FROM wharf_data WHERE utime >= " . (time()-24*3600*14) . "ORDER BY utime asc";
+            " FROM solar_data  WHERE utime >= " . (time()-24*3600*14) . " ORDER BY utime asc";
     }
-
+    
     query_and_emit_csv($fobj, $query);
 
 }
@@ -87,41 +91,37 @@ function make_wind_csv( $fobj, $start_ts, $end_ts){
     //needs way more work than SOLAR OR WEATHER
 }
 
-
-
-
 header("Content-type: txt/csv");
 header("Content-Disposition: attachment; filename=test.csv");
 include('db_credentials.php');
 include_once('helpers.php');
 
-$dataNeeded = $_GET['type'];
-echo("Data Needed: $dataNeeded");
-
 //Pull UTC and unix timestamps from GET options
 $start_date_UTC = $_GET['start'];
 $end_date_UTC   = $_GET['end'];
+$type_of_csv    = $_GET['type'];
 $start_ts       = strtotime($start_date_UTC);
 $end_ts         = strtotime($end_date_UTC);
-
 
 
 //Connect to database and execute query
 $conn = mysql_connect($host, $user, $pw) or die('Could not connect: ' . mysql_error());
 mysql_select_db($db, $conn) or die('No Luck: ' . mysql_error() . "\n");
 
-
 /* open file */
 $output=fopen('php://output','w');
-make_weather_csv($output, $start_ts, $end_ts)
 
-/* process solar_data table */
+if ( strtolower($type_of_csv) == 'weather' ) {
+    header("Content-type: txt/csv");
+    header("Content-Disposition: attachment; filename=weather.csv");
+    make_weather_csv($output); 
+} elseif ( strtolower($type_of_csv) == 'solar') {
+    header("Content-type: txt/csv");
+    header("Content-Disposition: attachment; filename=solar.csv");
+    make_solar_csv($output);
 
-/* $resp = mysql_query($solar_query);
-
-while ($row = mysql_fetch_assoc($resp)) {
-   if ($row['AmpsOut'] != 999 && $row['BatVolts'] != 999) array_push($json['solar'], array($row['utime']*1000, (float) ($row['AmpsOut']*$row['BatVolts'])));
 }
-$json['windDir'] = $windBins;
-echo json_encode($json);*/
+//make_weather_csv($output, $start_ts, $end_ts);
+fclose($output);
+
 ?>
